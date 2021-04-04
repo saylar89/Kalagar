88729a136ac05b34281e39767d3cbdf45ed3b14f
Aslan Ashtiani <saylar.design@gmail.com>
2021-04-04 15:11:27 +0430

    first

---
+++ index.js
@@ -0,0 +1,12 @@
+console.log("Aslan");
+
+let person = {
+  name: "Amir",
+  age: 35,
+};
+
+person.name = "Morteza";
+//zamani ke nemidanim value key ma chi hast az [] estefade mikonim*/
+person["name"] = "Ali";
+
+console.log(person);
