5600e5f8679b42230576719e0ffb8273ab8912f5
Aslan Ashtiani <saylar.design@gmail.com>
2021-04-05 18:00:27 +0430

    adding and deleting array element

---
+++ 6-Adding and deleting array elements.js
@@ -0,0 +1,16 @@
+let a = [1, 2, 3, 4, 5];
+
+a.push(6);
+console.log(a);
+
+a.unshift(0);
+console.log(a);
+
+a.pop();
+console.log(a);
+
+a.shift();
+console.log(a);
+
+delete a[0];
+console.log(a);
