5d3559d219dcb5150868ff6ba9626be05dab8b08
Aslan Ashtiani <saylar.design@gmail.com>
2021-04-05 17:52:39 +0430

    sparse array

---
+++ 5-Sparse array and length method.js
@@ -0,0 +1,11 @@
+//Sparse array is an array which one of its index is empty at least
+
+let sparse = new Array(5);
+console.log(sparse);
+//variable sparse is a sparse array
+
+let b = [1, 2, 3, 4, 5];
+b.length = 3;
+console.log(b);
+b.length = 5;
+console.log(b);
