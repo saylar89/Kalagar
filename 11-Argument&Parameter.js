300e6cb2c17913e2385f8e1f20807a4e716e333e
Aslan Ashtiani <saylar.design@gmail.com>
2021-04-06 14:26:04 +0430

    Argument and parameter

---
+++ 11-Argument&Parameter.js
@@ -0,0 +1,36 @@
+function logFullName(fName, lName) {
+  console.log(fName + " " + lName);
+}
+
+logFullName("Reza", "Zand"); //return Reza Zand
+logFullName("Ali"); // return Ali undefined // because we didn't write second param
+
+//for solving this problem
+
+//first solution
+function logFullName2(fName, lName) {
+  fName = fName || "Dear";
+  lName = lName || "stranger";
+  console.log(fName + " " + lName);
+}
+
+logFullName2(); //return Dear stranger
+logFullName2("Ali"); // return Ali stranger
+
+//second solution (recommended)
+
+function logFullName3(fName = "Dear", lName = "stranger") {
+  console.log(fName + " " + lName);
+}
+
+logFullName3(); //return Dear stranger
+logFullName3("Ali"); // return Ali stranger
+
+// Rest parameter
+//... is rest parameter
+function logFullName4(a, b, ...c) {
+  for (let x in c) {
+    console.log(x);
+  }
+}
+logFullName4("sara", "aram", 1, 2, 3, 4, 5, 6);
